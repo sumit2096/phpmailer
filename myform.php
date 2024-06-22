07cdda7d342fbb0e56c23835ec3020de8334cbe8
sumit2096 <sumit.naskar1234@gmail.com>
2024-06-22 16:02:19 +0530

    Add files via upload

---
+++ myform.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<div class="box">
+    <form action="send.php" method="post">
+    <input type="email" name="" id="">
+    <input type="submit" name="" value="Submit" id="">
+    </form>
+</div>
+</body>
+</html>
